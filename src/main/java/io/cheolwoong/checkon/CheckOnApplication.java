b87140b998421fd65985c7c76df595d1a2a1694a
한승우 <tmddn_00@naver.com>
2026-07-21 18:14:57 +0900

    first commit

---
+++ src/main/java/io/cheolwoong/checkon/CheckOnApplication.java
@@ -0,0 +1,13 @@
+package io.cheolwoong.checkon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckOnApplication.class, args);
+	}
+
+}
