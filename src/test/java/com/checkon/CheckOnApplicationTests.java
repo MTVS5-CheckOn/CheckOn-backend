661eabe8bd7044e8154c8db45ace911873b2958f
한승우 <tmddn_00@naver.com>
2026-07-27 18:30:28 +0900

    [Test] uuidv7 함수 버전 테스트

--- src/test/java/com/checkon/CheckOnApplicationTests.java
+++ src/test/java/com/checkon/CheckOnApplicationTests.java
@@ -1,13 +1,42 @@
 package com.checkon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Testcontainers
 class CheckOnApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer POSTGRESQL = new PostgreSQLContainer("postgres:18.4");
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	CheckOnApplicationTests(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void postgresqlSupportsUuidV7() {
+		Boolean isVersion7 = jdbcTemplate.queryForObject(
+			"SELECT uuid_extract_version(uuidv7()) = 7",
+			Boolean.class
+		);
+
+		assertTrue(Boolean.TRUE.equals(isVersion7));
 	}
 
 }
